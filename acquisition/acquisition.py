22ed213c973203a135831024b5fb12699edf1bb6
aleytia <jarek13705@gmail.com>
2019-01-13 00:51:38 -0500

    Completed ConfigHandler for the Acquisition Unit, added config design

---
+++ acquisition/acquisition.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+from src.config import ConfigHandler
+
+def main():
+    c = ConfigHandler()
+
+
+if __name__ == "__main__":
+    main()
---
+++ acquisition/src/config.py
@@ -0,0 +1,433 @@
+import sys
+import os
+
+import json
+import yaml
+
+import pprint as pp
+
+import requests # For fetching web configs
+
+# File Extension Definitions
+YAML_EXT = ['.yaml', '.yml']
+JSON_EXT = ['.json']
+
+
+class ConfigHandler:
+    """
+    ConfigHandler deals with loading and extracting configurations. 
+    """
+
+    def __init__(self, cpath="config.yml"):
+        """
+        Args:
+            cpath - A path that points to the config file. If not specified,
+            the application will load the regular config.yml.
+        """
+
+        # Get the absolute path of the config.yml path for all use cases.
+        cpath_abs = os.path.abspath(cpath)
+        # Load the config into a dict Object
+        initial_conf = self._load_local_config(cpath_abs)
+
+        """
+        Variables
+        """
+        self._web_config_url = None # The URL if a config will be loaded from the web
+        self._conf = None # The real config to parse all other vars from
+        self._web_conf_use = False # Whether or not using web-style conf
+
+        self.watch_folder = None # The watch folder of the string
+
+        self.encoders_always = None # The encoding endpoints, always
+        self.encoders_sequential = None # The encoding endpoints, sequential
+        self.notifiers_always = None # The notification endpoints, always
+        self.notifiers_sequential = None # The notification endpoints, sequential
+        self.distributors_always = None # The distribution endpoints, always
+        self.distributors_sequential = None # The distribution endpoints, sequential
+
+        self.delimiter = None # The delimiter used by inotify !!IMPORTANT
+        self.verbose = False # Whether or not we are printing verbosely
+
+        # ---------------- #
+
+        # First, we want to load the web config URL if it exists.
+        self._web_config_url = self._load_web_config_url(initial_conf)
+        # If it does exist, we want to open the URL 
+        if self._web_config_url:
+            # If there is a web url, then load it from here
+            self._conf = self._load_conf_from_web(self._web_config_url)
+            self._web_conf_use = True
+        # Else load it from the regular file 
+        else:
+            self._conf = initial_conf
+            self._web_conf_use = False
+
+        # ---------------- #
+        # Populate the rest of the variables now
+        # ---------------- #
+        self.watch_folder = self._load_watch_folder(self._conf, self._web_conf_use)
+
+        self.encoders_always = self._load_endpoints_encoders_always(self._conf, self._web_conf_use)
+        self.encoders_sequential = self._load_endpoints_encoders_sequential(self._conf, self._web_conf_use)
+        self.notifiers_always = self._load_endpoints_notifiers_always(self._conf, self._web_conf_use)
+        self.notifiers_sequential = self._load_endpoints_notifiers_sequential(self._conf, self._web_conf_use)
+        self.distributors_always = self._load_endpoints_distributors_always(self._conf, self._web_conf_use)
+        self.distributors_sequential = self._load_endpoints_distributors_sequential(self._conf, self._web_conf_use)
+
+        self.delimiter = self._load_system_delimiter(self._conf, self._web_conf_use)
+        self.verbose = self._load_system_verbose(self._conf, self._web_conf_use)
+
+    def _load_local_config(self, cpath_abs):
+        """
+        Loads the config path into a parsable dict.
+
+        Args:
+            cpath_abs: The absolute path of the config file, path only
+
+        Returns: A dict representation of the config file.
+        """
+
+        # Detremine if we're loading a YAML or JSON file
+        _, file_ext = os.path.splitext(cpath_abs)
+
+        if file_ext in YAML_EXT:
+            with open(cpath_abs, 'r') as cfyml:
+                try:
+                    return yaml.load(cfyml)
+                except:
+                    pass
+                    # TODO: print statement here for loading config error
+
+        elif file_ext in JSON_EXT:
+            with open(cpath_abs, 'r') as cfjson:
+                try:
+                    return json.load(cfjson)
+                except:
+                    pass
+                    # TODO: print statement here for loading config error
+
+        # If we've reached this point, then print some 
+        # TODO: print statement for loading config error
+
+
+    def _load_web_config_url(self, conf):
+        """
+        Loads the web config URL from the config.
+        This is the ONLY function that doesn't have multiple behaviors 
+            (between web and local)
+ 
+        Args:
+            conf: A dictionary representation of the Config file
+
+        Returns: The string representing the web-config entry
+        """
+        return conf['web-config']
+
+
+    def __determine_url_extension(self, url):
+        """
+        Determines the file extension from the URL of the web.
+        Because we're not using headers, we need to manually scan the
+        string itself.
+
+        Args:
+            url: The url of the web conf
+
+        Returns: The extension of the url. Includes the "."
+        """
+        for ext in YAML_EXT:
+            if url.endswith(ext):
+                return ext
+
+        for ext in JSON_EXT:
+            if url.endswith(ext):
+                return ext
+
+        # TODO: Raise an exception if we reach this point cause no
+        # suitable extension was found
+
+
+    def _load_conf_from_web(self, url):
+        """
+        Retrieves the config from the web and returns it as a dict object
+
+        Args:
+            url: The url of the web conf. Should be a direct link to a 
+            yaml or json file. 
+
+        Returns: The web config, as a Dict object
+        """
+
+        # Figure out what extension we're working with
+        file_ext = self.__determine_url_extension(url)
+        # Fetch the request itself
+        webconf = requests.get(url)
+
+        # Run it through YAML or JSON accordingly
+        if file_ext in YAML_EXT:
+            try:
+                return yaml.load(webconf.text)
+            except:
+                pass
+                # TODO: print statement here for loading config error
+
+        elif file_ext in JSON_EXT:
+            try:
+                return json.loads(webconf.text)
+            except:
+                pass     
+                # TODO: print statement here for loading config error
+
+    def _load_watch_folder(self, conf, web):
+        """
+        Determines the watch folder for this application.
+
+        Params:
+            conf: self._conf, which represents a dict object
+                of the loaded conf
+            web: A boolean value which indicates if the web conf
+                is being used (default: local)
+
+        Returns: The watch folder string.
+        """
+
+        # TODO: Return if web
+        if web:
+            pass
+
+        # Return the local, since we're not using the web conf
+        return conf['watch-folder']
+
+
+    def _load_endpoints_encoders_always(self, conf, web):
+        """
+        Finds the ALWAYS encoder endpoints.
+
+        Params:
+            conf: self._conf, which represents a dict object
+                of the loaded conf
+            web: A boolean value which indicates if the web conf
+                is being used (default: local)
+
+        Returns: A list of lists, where each inside list represents one endpoint
+            entry. Index 0 is URL, Index 1 (optional) is an Authorization key.
+        """
+        if web:
+            pass
+
+        # Return from the local config
+        return conf['endpoints']['encoders']['always']
+
+
+    def _load_endpoints_encoders_sequential(self, conf, web):
+        """
+        Finds the SEQUENTIAL encoder endpoints.
+
+        Params:
+            conf: self._conf, which represents a dict object
+                of the loaded conf
+            web: A boolean value which indicates if the web conf
+                is being used (default: local)
+
+        Returns: A list of list of lists. 
+        For x in root list, y in x in order represents URLs in order to 
+        try until a single request succeeds.
+        Follows standard index 0/1 url/auth key(opt)
+        """
+        if web:
+            pass
+
+        # Return from the local config
+        return conf['endpoints']['encoders']['sequential']
+
+
+    def _load_endpoints_notifiers_always(self, conf, web):
+        """
+        Finds the ALWAYS notification endpoints.
+
+        Params:
+            conf: self._conf, which represents a dict object
+                of the loaded conf
+            web: A boolean value which indicates if the web conf
+                is being used (default: local)
+
+        Returns: A list of lists, where each inside list represents one endpoint
+            entry. Index 0 is URL, Index 1 (optional) is an Authorization key.
+        """
+        if web:
+            pass
+
+        # Return from the local config
+        return conf['endpoints']['notifiers']['always']
+
+    def _load_endpoints_notifiers_sequential(self, conf, web):
+        """
+        Finds the SEQUENTIAL notification endpoints.
+
+        Params:
+            conf: self._conf, which represents a dict object
+                of the loaded conf
+            web: A boolean value which indicates if the web conf
+                is being used (default: local)
+
+        Returns: A list of list of lists. 
+        For x in root list, y in x in order represents URLs in order to 
+        try until a single request succeeds.
+        Follows standard index 0/1 url/auth key(opt)
+        """
+        if web:
+            pass
+
+        # Return from the local config
+        return conf['endpoints']['notifiers']['sequential']
+
+    def _load_endpoints_distributors_always(self, conf, web):
+        """
+        Finds the ALWAYS notification endpoints.
+
+        Params:
+            conf: self._conf, which represents a dict object
+                of the loaded conf
+            web: A boolean value which indicates if the web conf
+                is being used (default: local)
+
+        Returns: A list of lists, where each inside list represents one endpoint
+            entry. Index 0 is URL, Index 1 (optional) is an Authorization key.
+        """
+        if web:
+            pass
+
+        # Return from the local config
+        return conf['endpoints']['distributors']['always']
+
+    def _load_endpoints_distributors_sequential(self, conf, web):
+        """
+        Finds the SEQUENTIAL notification endpoints.
+
+        Params:
+            conf: self._conf, which represents a dict object
+                of the loaded conf
+            web: A boolean value which indicates if the web conf
+                is being used (default: local)
+
+        Returns: A list of list of lists. 
+        For x in root list, y in x in order represents URLs in order to 
+        try until a single request succeeds.
+        Follows standard index 0/1 url/auth key(opt)
+        """
+        if web:
+            pass
+
+        # Return from the local config
+        return conf['endpoints']['distributors']['sequential']
+
+    def _load_system_delimiter(self, conf, web):
+        """
+        Finds the Delimiter set in Config and used by inotify
+
+        Params:
+            conf: self._conf, which represents a dict object
+                of the loaded conf
+            web: A boolean value which indicates if the web conf
+                is being used (default: local)
+
+        Returns: The delimiter, as a string
+        """
+        if web:
+            pass
+
+        # Return from the local config
+        return conf['system']['delimiter']
+
+    def _load_system_verbose(self, conf, web):
+        """
+        Finds the Boolean Verbose option set in Config
+
+        Params:
+            conf: self._conf, which represents a dict object
+                of the loaded conf
+            web: A boolean value which indicates if the web conf
+                is being used (default: local)
+
+        Returns: A boolean value for verbosity usage
+        """
+        if web:
+            pass
+
+        # Return from the local config
+        return conf['system']['verbose']
+
+    # Getter methods
+
+    def get_watch_folder(self):
+        """
+        Returns the watch folder, as a string
+        """
+        return self.watch_folder
+
+    def get_encoders(self):
+        """
+        Returns the encoders as a tuple (always, sequential)
+        """
+        return (self.encoders_always, self.encoders_sequential)
+
+    def get_encoders_always(self):
+        """
+        Returns the always encoder endpoints as a list of lists
+        """
+        return self.encoders_always
+
+    def get_encoders_sequential(self):
+        """
+        Returns the sequential encoder endpoints as a list of list of lists
+        """
+        return self.encoders_sequential
+
+    def get_notifiers(self):
+        """
+        Returns the notifiers as a tuple (always, sequential)
+        """
+        return (self.notifiers_always, self.notifiers_sequential)
+
+    def get_notifiers_always(self):
+        """
+        Returns the always notifier endpoints as a list of lists
+        """
+        return self.notifiers_always
+
+    def get_notifiers_sequential(self):
+        """
+        Returns the sequential notifier endpoints as a list of list of lists
+        """
+        return self.notifiers_sequential
+
+    def get_distributors(self):
+        """
+        Returns the distributors as a tuple (always, sequential)
+        """
+        return (self.distributors_always, self.distributors_sequential)
+
+    def get_distributors_always(self):
+        """
+        Returns the always distributors endpoints as a list of lists
+        """
+        return self.distributors_always
+
+    def get_distributors_sequential(self):
+        """
+        Returns the sequential distributors endpoints as a list of list of lists
+        """
+        return self.distributors_sequential
+
+    def get_delimiter(self):
+        """
+        Returns the delimiter used by inotify as a string
+        """
+        return self.delimiter
+
+    def get_verbose(self):
+        """
+        Returns a boolean representing whether or not to verbose print
+        """
+        return self.verbose
