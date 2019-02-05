cb0e7a403a0ceccf688a73e333bf0550d63870fc
aleytia <jarek13705@gmail.com>
2019-02-04 21:29:38 -0500

    Updated prints and request handling and a lot of other stuff

--- distributor/distribution.py
+++ distributor/distribution.py
@@ -19,10 +19,62 @@ from threading import Thread
 from queue import Queue
 
 from src.config_handler import ConfigHandler
+from src.auth_handler import AuthHandler
+from src.request_handler import RequestHandler
+from src.print_handler import PrintHandler
+from src.prints.distribution_prints import DistributionPrints
 
 app = Flask(__name__)
 
 episode_job_queue = Queue()
 
 c = ConfigHandler()
+p = PrintHandler(c)
+logger = p.get_logger()
+dp = DistributionPrints(p.Colors())
+a = AuthHandler(p)
 
+def distribute_worker():
+    """
+    Represents a single thread that is continuously scanning for a new distribution job to handle.
+    Once it finds one, it takes it and procsses it.
+    """
+
+    while True:
+        new_request = episode_job_queue.get()
+
+        episode_job_queue.task_done()
+        logger.warning(dp.JOB_COMPLETE)
+        print()
+
+@app.route("/distribute", methods=['POST'])
+def distribute():
+
+    # Headers must be passed in separately, so the request isn't processeed
+    # before it's confirmed to be authorized
+
+    print()
+    logger.warning(dp.NEW_REQUEST)
+
+    a.refresh()
+    status = a.authorize(request.headers)
+
+    if not status:
+        return "Unauthorized request", 403
+
+    r = RequestHandler(request, p)
+    episode_job_queue.put(r)
+
+    return str(status), 200
+
+if __name__ == "__main__":
+
+    logger.warning(dp.WORKER_SPAWN.format(c.get_distributor_jobs()))
+
+    # Spawn the number of worker threads in the background
+    for _ in range(c.get_distributor_jobs()):
+        distribute = Thread(target=distribute_worker)
+        distribute.daemon = True
+        distribute.start()
+
+    app.run(host='0.0.0.0', port=c.get_listen_port(), debug=True)
--- encoder/encoder.py
+++ encoder/encoder.py
@@ -24,6 +24,7 @@ from src.request_handler import RequestHandler
 from src.os_handler import OSHandler
 from src.network_handler import NetworkHandler
 from src.print_handler import PrintHandler
+from src.prints.encode_prints import EncodePrints
 
 # Let flask just use the default name
 app = Flask(__name__)
@@ -33,13 +34,15 @@ episode_job_queue = Queue()
 
 c = ConfigHandler()
 p = PrintHandler(c)
+logger = p.get_logger()
+ep = EncodePrints(p.Colors())
 a = AuthHandler(p)
 
 
 def encode_worker():
     """
     Represents a single thread that is continuously scanning for a new encode job to do.
-    Once it finds one, it takes it and proceses it.
+    Once it finds one, it takes it and processes it.
     """
     while True:
 
@@ -56,12 +59,13 @@ def encode_worker():
         o.cleanup()
 
         # Create the NetworkHandler to send out notifications
-        #n = NetworkHandler(c, new_request, p, ofile_name, ofile_size)
-        #n.notify_notifiers()
-        #n.notify_distributors()
+        n = NetworkHandler(c, new_request, p, ofile_name, ofile_size)
+        n.notify_notifiers()
+        n.notify_distributors()
 
         # Mark the job as done
         episode_job_queue.task_done()
+        logger.warning(ep.JOB_COMPLETE)
         print()
 
 
@@ -70,6 +74,9 @@ def encode():
 
     # Headers must be passed in separately, so the request isn't processed
     # before it's confirmed to be authorized
+
+    print()
+    logger.warning(ep.NEW_REQUEST)
 
     a.refresh() # Refresh in case any auths were made
     status = a.authorize(request.headers)
@@ -83,6 +90,8 @@ def encode():
     return str(status), 200
 
 if __name__ == "__main__":
+
+    logger.warning(ep.WORKER_SPAWN.format(c.get_encode_encode_jobs()))
 
     # Spawn the number of worker threads in the background
     for _ in range(c.get_encode_encode_jobs()):
