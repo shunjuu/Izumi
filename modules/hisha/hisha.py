d6560a1c66dc6ecb2b6dba00032d4aa0de7834ce
aleytia <jarek13705@gmail.com>
2019-05-14 15:17:01 -0400

    Completed HishaInfo

--- modules/hisha/hisha.py
+++ modules/hisha/hisha.py
@@ -4,10 +4,220 @@ import os
 import re
 import logging
 import requests
+import pprint
 
-import hisha_constants as hc
-        
 logging.basicConfig(format="[%(name)s] (%(levelname).4s) %(message)s")
+
+ANILIST_API_URL = "https://graphql.anilist.co"
+
+ANILIST_SINGLE_QUERY = '''
+query ($search: String, $status: MediaStatus) {
+    Media(search: $search, status: $status, type: ANIME) {
+        id
+        idMal
+        episodes
+        duration
+        popularity
+        averageScore
+        bannerImage
+        coverImage {
+            large
+        }
+        title {
+            userPreferred
+            native
+            english
+            romaji
+        }
+        startDate {
+            year
+        }
+        endDate {
+            year
+        }
+    }
+}
+'''
+
+ANILIST_PAGE_QUERY = '''
+query ($search: String, $status: MediaStatus) {
+    Page (page: 1) {
+        pageInfo {
+            total
+            currentPage
+        }
+
+        media(search: $search, status: $status, type: ANIME) {
+            id
+            idMal
+            episodes
+            duration
+            popularity
+            averageScore
+            bannerImage
+            coverImage {
+                large
+            }
+            title {
+                userPreferred
+                native
+                english
+                romaji
+            }
+            startDate {
+                year
+            }
+            endDate {
+                year
+            }
+        }
+    }
+}
+'''
+
+class HishaInfo:
+    """
+    This is a class that holds the information for a returned show from Hisha.
+    Return this instead of a raw dict to streamline data access.
+    """
+
+    def __init__(self):
+        # Set the default values here
+        self._id = 0
+        self._idMal = 0
+        self._episodes = -1
+        self._duration = -1
+        self._popularity = -1
+        self._averageScore = -1
+        self._bannerImage = ""
+        self._coverImage = ""
+        self._title_userPreferred = "Unknown"
+        self._title_native = "Unknown"
+        self._title_english = "Unknown"
+        self._title_romaji = "Unknown"
+        self._startDate_year = -1
+        self._endDate_year = -1
+
+    @property
+    def id(self):
+        return self._id
+
+    @id.setter
+    def id(self, val):
+        if val: self._id = val
+
+    @property
+    def idMal(self):
+        return self._idMal
+
+    @idMal.setter
+    def idMal(self, val):
+        if val: self._idMal = val
+    
+    @property
+    def episodes(self):
+        return self._episodes
+
+    @episodes.setter
+    def episodes(self, val):
+        if val: self._episodes = val
+
+    @property
+    def duration(self):
+        return self._duration
+    
+    @duration.setter
+    def duration(self, val):
+        if val: self._duration = val
+
+    @property
+    def popularity(self):
+        return self._popularity
+
+    @popularity.setter
+    def popularity(self, val):
+        if val: self._popularity = val
+
+    @property
+    def averageScore(self):
+        return self._averageScore
+    
+    @averageScore.setter
+    def averageScore(self, val):
+        if val: self._averageScore = val 
+
+    @property
+    def bannerImage(self):
+        return self._bannerImage
+   
+    @bannerImage.setter
+    def bannerImage(self, val):
+        if val: self._bannerImage = val 
+
+    @property
+    def coverImage(self):
+        return self._coverImage
+    
+    @coverImage.setter
+    def coverImage(self, val):
+        if val: self._coverImage = val
+
+    @property
+    def title(self):
+        """Title as a property itself is treated as userPreferred"""
+        return self._title_userPreferred
+    
+    @title.setter
+    def title(self, val):
+        if val: self._title_userPreferred = val
+
+    @property
+    def title_userPreferred(self):
+        return self._title_userPreferred
+   
+    @title_userPreferred.setter
+    def title_userPreferred(self, val):
+        if val: self._title_userPreferred = val 
+
+    @property
+    def title_native(self):
+        return self._title_native
+    
+    @title_native.setter
+    def title_native(self, val):
+        if val: self._title_native = val
+
+    @property
+    def title_english(self):
+        return self._title_english
+    
+    @title_english.setter
+    def title_english(self, val):
+        if val: self._title_english = val
+
+    @property
+    def title_romaji(self):
+        return self._title_romaji
+    
+    @title_romaji.setter
+    def title_romaji(self, val):
+        if val: self._title_romaji = val
+
+    @property
+    def startYear(self):
+        return self._startDate_year
+
+    @startYear.setter
+    def startYear(self, val):
+        if val: self._startDate_year = val
+
+    @property
+    def endYear(self):
+        return self._endDate_year
+
+    @endYear.setter
+    def endYear(self, val):
+        if val: self._endDate_year = val
 
 class Hisha:
     """
@@ -19,9 +229,9 @@ class Hisha:
 
     def __init__(self, loglevel=logging.INFO):
 
-        self._API_URL = hc.ANILIST_API_URL
-        self._SINGLE_QUERY = hc.ANILIST_SINGLE_QUERY
-        self._PAGE_QUERY = hc.ANILIST_PAGE_QUERY
+        self._API_URL = ANILIST_API_URL
+        self._SINGLE_QUERY = ANILIST_SINGLE_QUERY
+        self._PAGE_QUERY = ANILIST_PAGE_QUERY
 
         self._logger = logging.getLogger("Hisha")
         self._logger.setLevel(loglevel)
@@ -97,9 +307,10 @@ class Hisha:
         for title in info['Media']['title'].values():
             if self._check_equality_regex(search, title):
                 self._logger.debug("Matched {} to {}".format(search, title))
-                return info
+                return info['Media']
 
         # But if none of the titles match, return None
+        self._logger.debug("Didn't match {} in {}".format(search, status))
         return None
 
     def _page_search(self, search, status):
@@ -126,6 +337,42 @@ class Hisha:
         # If there are no matches, return None
         return None
 
+    def _create_hisha_info(self, show, title):
+        """
+        Creates a HishaInfo object from a provided show json
+        Params:
+            show - a dict that represents Anilist show response, or None
+            title - the name of the show (for when it's not provided)
+
+        Returns a HishaInfo object
+        """
+
+        hishaInfo = HishaInfo()
+
+        if show is None:
+            # If the show doesn't exist, set title and use defaults
+            hishaInfo.title = title 
+            hishaInfo.title_native = title
+            hishaInfo.title_english = title
+            hishaInfo.title_romaji = title
+        else:
+            # Don't need to check for None values - setters will handle it
+            hishaInfo.id = show['id']
+            hishaInfo.idMal = show['idMal']
+            hishaInfo.episodes = show['episodes']
+            hishaInfo.duration = show['duration']
+            hishaInfo.popularity = show['popularity']
+            hishaInfo.averageScore = show['averageScore']
+            hishaInfo.bannerImage = show['bannerImage']
+            hishaInfo.coverImage = show['coverImage']['large']
+            hishaInfo.title = show['title']['userPreferred']
+            hishaInfo.title_native = show['title']['native']
+            hishaInfo.title_english = show['title']['english']
+            hishaInfo.title_romaji = show['title']['romaji']
+            hishaInfo.startYear = show['startDate']['year']
+            hishaInfo.endYear = show['endDate']['year']
+
+        return hishaInfo
 
     def search(self, show):
         """
@@ -133,24 +380,25 @@ class Hisha:
         """
         airing = self._single_search(show, "RELEASING")
         if airing:
-            # Create a Hisha object and return it
-            return airing
+            self._logger.info("Creating HishaInfo for {} in RELEASING".format(show))
+            return self._create_hisha_info(airing, show)
 
         finished = self._page_search(show, "FINISHED")
         if finished:
-            # Create a Hisha object and return it
-            return finished
+            self._logger.info("Creating HishaInfo for {} in FINISHED".format(show))
+            return self._create_hisha_info(finished, show)
 
         not_yet_released = self._single_search(show, "NOT_YET_RELEASED")
         if not_yet_released:
-            # Create a Hisha object and return it
-            return not_yet_released
+            self._logger.info("Creating HishaInfo for {} in NOT_YET_RELEASED".format(show))
+            return self._create_hisha_info(not_yet_released, show)
 
         # None of the three found a result, so create a dummy Hisha object and return it
-        return # dummy
+        self._logger.info("Creating HishaInfo for {} with default values".format(show))
+        return self._create_hisha_info(None, show)
 
 
 if __name__ == "__main__":
 
     h = Hisha(loglevel=logging.DEBUG)
-    print(h.search(sys.argv[1]))
+    pprint.pprint(h.search(sys.argv[1]).title)
