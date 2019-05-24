f035e49b9538fb897870ff27205ee0f98f186499
aleytia <jarek13705@gmail.com>
2019-05-24 14:25:54 -0400

    Yet another fix - ignore spaces for name checking

--- modules/hisha/hisha.py
+++ modules/hisha/hisha.py
@@ -246,8 +246,8 @@ class Hisha:
 
         try:
             # Anilist sometimes has weird leading/trailing spaces
-            re_str1 = re.sub(r'[^\w\s]','', str1).strip()
-            re_str2 = re.sub(r'[^\w\s]','', str2).strip()
+            re_str1 = re.sub(r'[^\w]','', str1)
+            re_str2 = re.sub(r'[^\w]','', str2)
             return bool(re_str1 == re_str2)
         except:
             return False
