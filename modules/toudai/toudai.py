2dff9aba61b538efdb5572f557fc9ccb007a172b
Aleytia <jarek13705@gmail.com>
2019-10-28 21:02:03 -0700

    Add logging features

--- modules/toudai/toudai.py
+++ modules/toudai/toudai.py
@@ -1,8 +1,7 @@
-
 from collections import OrderedDict
+from logging import basicConfig, getLogger, Logger, DEBUG
 from os import listdir, walk
 from os.path import exists, getsize, isdir, isfile, join
-from pprint import pprint
 from queue import Queue
 from signal import signal, SIGINT
 from sys import exit
@@ -13,15 +12,40 @@ from typing import List, Tuple
 def sigint_handler(sig, frame):
     print("\rKill command detected, exiting...")
     exit()
-signal(SIGINT, sigint_handler)
 
 class Toudai:
+
+    class ToudaiEvent:
+        """
+        The event that will be added to the Toudai Queue for processing.
+        """
+        def __init__(self, fullpath: str, root: str, name: str, watch: str):
+            self._fullpath: str = fullpath # The full path of the new file that Toudai was watching
+            self._root: str = root # Root path of the new file
+            self._name: str = name # Name of the new file, not including the path
+            self._watch: str = watch # Path that Toudai is watching
+
+        @property
+        def fullpath(self) -> str:
+            return self._fullpath
+        
+        @property
+        def root(self) -> str:
+            return self._root
+        
+        @property
+        def name(self) -> str:
+            return self._name
+        
+        @property
+        def watch(self) -> str:
+            return self._watch
 
     class TrackingFile:
         """
         Internal class to store information about files we are tracking
         """
-        def __init__(self, file):
+        def __init__(self, file: Tuple[str, str, str]):
             self.file: Tuple[str, str, str] = file # Holds the tuple tracked in Watch()
             self.filesize: int = getsize(self.file[0]) # Byte size of the file Toudai is tracking
             self.added: bool = False # A boolean to track if this TF was added into the Queue
@@ -52,7 +76,7 @@ class Toudai:
         return -1
 
     @staticmethod
-    def watch(path: str, queue: Queue, interval: int = 5) -> None:
+    def watch(path: str, queue: Queue, logger: Logger, interval: int = 5, ) -> None:
         """
         Watches a given path and tracks all the files in that path directory.
         Checks every interval, and once a file has unchanged, append the event 
@@ -63,9 +87,11 @@ class Toudai:
             queue: A global job queue for Izumi
             interval: Time, in seconds, to wait
         """
+        signal(SIGINT, sigint_handler)
+
         if not exists(path) or not isdir(path):
-            print("Invalid path passed in")
-            # TODO: Log out some errors and exit
+            logger.critical("An invalid path was provided to Toudai. The system will now exit.")
+            exit()
         
         global_tracker = OrderedDict()
         while True:
@@ -76,45 +102,46 @@ class Toudai:
             for fullpath, tf in global_tracker.items():
                 # Ignore files we already processed
                 if tf.added:
+                    logger.debug("File {} has already been added to the queue, skipping".format(fullpath))
                     continue
 
                 curr_file_idx = Toudai.idx_curr_files(fullpath, curr_files)
                 if curr_file_idx >= 0:
-                    print("{} exists, process it now".format(fullpath))
-                    # Do a size comparison - unchanged sizes are done, changed sizes need to wait again
+                    logger.debug("Found {} in current files and global files, beginning processing for it".format(fullpath))
 
+                    # Do a size comparison - unchanged sizes are done, changed sizes need to wait again
                     global_filesize = tf.filesize
                     curr_filesize = getsize(fullpath) # It's the same path as the curr path
 
-                    print("two sizes: {} bytes vs {} bytes".format(global_filesize, curr_filesize))
+                    logger.debug("Global filesize: {}, curr filesize: {}".format(global_filesize, curr_filesize))
 
                     if global_filesize != curr_filesize:
-                        print("Found a filesize change for {}".format(fullpath))
+                        logger.debug("Filesize for {} changed, updating size and continuing tracking".format(fullpath))
                         global_tracker[fullpath].filesize = curr_filesize
                         continue
                     else:
-                        print("No size differences were found, adding to queue")
+                        logger.info("Adding {} to the queue".format(fullpath))
+                        queue.put(Toudai.ToudaiEvent(tf.file[0], tf.file[1], tf.file[2], path))
                         global_tracker[fullpath].added = True
-
                 else:
+                    logger.debug("File {} was not found in the current tracker but was in global, removing from global".format(fullpath))
                     global_tracker.pop(fullpath)
-                    print("Deleted {} from the global tracker".format(fullpath))
-
-            print()
-
 
             # Add new files in curr_files to the global_tracker
             for file in curr_files:
                 if file[0] not in global_tracker:
+                    logger.info("Adding {} to tracker".format(file[0]))
                     global_tracker[file[0]] = Toudai.TrackingFile(file)
-                    print("added {} to global tracker".format(file[0]))
-            
 
-            print("---------------------------------------")
             # Finally, sleep
+            logger.debug("Sleeping {} seconds".format(interval))
             sleep(interval)
 
 if __name__ == "__main__":
     global_queue = Queue()
 
-    Toudai.watch("/Users/adieuri/Desktop/Watch", global_queue, 5)
+    basicConfig(format="[%(name)s] (%(levelname)s) %(asctime)s: %(message)s")
+    logger = getLogger("Toudai")
+    logger.setLevel(DEBUG)
+
+    Toudai.watch("/Users/adieuri/Desktop/Watch", global_queue, logger, 5)
