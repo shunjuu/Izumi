212b5c1223a68bfbda26dfb9810e9a4d446d3c5d
Aleytia <jarek13705@gmail.com>
2019-12-18 04:18:27 -0800

    Update Redis to be a bit more friendly

--- coreV5/redis/start.sh
+++ coreV5/redis/start.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+DOCKER_PORT="$(grep '^port = ' conf/izumi.toml | awk -F '= ' '{print $2}')"
+DOCKER_NAME="$(grep '^name = ' conf/izumi.toml | awk -F '"' '{print $2}')"
+
+echo "Starting Izumi Redis in Docker, mapping to port $DOCKER_PORT"
+echo "Docker container name is '$DOCKER_NAME'"
+
 docker run -d \
     -v $(pwd)/conf/redis.conf:/usr/local/etc/redis/redis.conf \
-    -p 6379:6379 \
-    --name redisdev \
-    redis redis-server /usr/local/etc/redis/redis.conf
+    -p "$DOCKER_PORT:6379" \
+    --name "$DOCKER_NAME" \
+    redis:5.0.7-alpine redis-server /usr/local/etc/redis/redis.conf
+
+docker logs -f "$DOCKER_NAME"
