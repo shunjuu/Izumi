6ef2db4efdf55a92c05dc0e145cbe50aeddef51e
Jun <jarek13705@gmail.com>
2020-04-17 00:05:31 -0700

    Add Traefik ignore to Redis (#105)
    * Add Traefik ignore to Redis
    
    * Fix indentation

--- coreV5/redis/start.sh
+++ coreV5/redis/start.sh
@@ -10,6 +10,7 @@ docker run -d \
     -v $(pwd)/conf/redis.conf:/usr/local/etc/redis/redis.conf \
     -p "$DOCKER_PORT:6379" \
     --name "$DOCKER_NAME" \
+    -l "traefik.enable=false" \
     redis:5.0.7-alpine redis-server /usr/local/etc/redis/redis.conf
 
 docker logs -f "$DOCKER_NAME"
